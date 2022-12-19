9900cfb54c683c30336c73bd145f2399b6cd1b95
Harika Rachamalla <harika.0293@gmail.com>
2022-12-19 20:03:45 +0530

    Updates

---
+++ precache-manifest.52f52c9bc6b5593ca59ad9a5ae366d5a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca4a6a3fec8202efa4d73f828e4cb23c",
+    "url": "/React-Redux-Furniture-Cart/index.html"
+  },
+  {
+    "revision": "49d4b5f350b98206ec65",
+    "url": "/React-Redux-Furniture-Cart/static/css/2.bd767988.chunk.css"
+  },
+  {
+    "revision": "8cd30d8ced9f2fdea0cf",
+    "url": "/React-Redux-Furniture-Cart/static/css/main.e99f06b1.chunk.css"
+  },
+  {
+    "revision": "49d4b5f350b98206ec65",
+    "url": "/React-Redux-Furniture-Cart/static/js/2.99a06452.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/React-Redux-Furniture-Cart/static/js/2.99a06452.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cd30d8ced9f2fdea0cf",
+    "url": "/React-Redux-Furniture-Cart/static/js/main.fa5bc099.chunk.js"
+  },
+  {
+    "revision": "ea45a4d512b423218974",
+    "url": "/React-Redux-Furniture-Cart/static/js/runtime-main.4e85745d.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/React-Redux-Furniture-Cart/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/React-Redux-Furniture-Cart/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/React-Redux-Furniture-Cart/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/React-Redux-Furniture-Cart/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/React-Redux-Furniture-Cart/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
